b3f5bbbb662b3fd471d4bd5b4a3ca91146576407
Tomasz Ogrodowski <tomasz.ogrodowski@itcraft.pl>
2025-09-18 11:31:08 +0200

    final grocery list implementation

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -7,40 +7,18 @@ class GroceryList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Your Groceries',
-          style: Theme.of(
-            context,
-          ).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),
-        ),
-        centerTitle: false,
-      ),
+      appBar: AppBar(title: const Text('Your Groceries')),
       body: ListView.builder(
         itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) {
-          final grocery = groceryItems[index];
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            child: Row(
-              children: [
-                Container(width: 24, height: 24, color: grocery.category.color),
-                const SizedBox(width: 30),
-                Text(
-                  grocery.name,
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                Spacer(),
-                Text(
-                  grocery.quantity.toString(),
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-          );
-        },
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.color,
+          ),
+          trailing: Text(groceryItems[index].quantity.toString()),
+        ),
       ),
     );
   }
